42534cb274bd9335dd5a67ee5463c1d2f40e13aa
ahmad0918 <ahmadhilmy0918>
2021-01-28 10:26:14 +0700

    namabah baru

--- app/src/main/java/com/example/myapplication/Act3.java
+++ app/src/main/java/com/example/myapplication/Act3.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static com.example.myapplication.R.layout.activity_act3;
+
 public class Act3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_act3);
+        setContentView(activity_act3);
     }
 }
