c54179d54d6d0c9676651933ce15d5feaad967a8
Petik <ahmadhilmy0918@gmail.com>
2021-01-28 09:43:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public  int jumlah = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView txtHW = findViewById(R.id.tv_hw);
+        TextView txtNama = findViewById(R.id.tv_nama);
+        TextView txtAlamat = findViewById(R.id.tv_alamat);
+        Button btnMove = findViewById(R.id.btn_pindah);
+
+        txtHW.setText("Selamat Datang");
+        txtNama.setText("Ahmad");
+        txtAlamat.setText("Ciwidey");
+
+        txtNama.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Anda menekan tulisan "+ txtNama.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        txtAlamat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Anda menekan tulisan alaamt",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        txtHW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jumlah++;
+                Toast.makeText(MainActivity.this, "ANDA CLICK GAMBAR !! " + jumlah + " kali",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( MainActivity.this, ActivityKedua.class);
+                intent.putExtra("nama_key", txtNama.getText());
+                intent.putExtra("alamat_key", txtAlamat.getText());
+                startActivity(intent);
+            }
+        });
+    }
+}
